2063c98c27b10bf34a6e97de27eb4ac1afac618c
申志明53843 <shenzhiming@tospur.com>
2019-04-15 13:50:56 +0800

    add readme
    add readme

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import find_packages, setup
 setup(
-    name='guacapyClientRest',
+    name='autotest',
     version='0.0.1',
-    description='Python REST API client for Guacamole 0.9.13 version',
-    author='author',#作者
-    author_email='xxxxxxxx@163.com',
-    url='https://github.com/xxxxx',
+    description='autotest',
+    author='szmgithubcode',#作者
+    author_email='13023297816@163.com',
+    url='https://github.com/szmgithubcode/uitestool/',
     #packages=find_packages(),
-    packages=['guacapyClientRest'],  #这里是所有代码所在的文件夹名称
+    packages=['autotest'],  #这里是所有代码所在的文件夹名称
     install_requires=['requests'],
 )
