5aeb4b0b4a90b5edda6b65e3978317a8a1779476
申志明53843 <shenzhiming@tospur.com>
2019-04-29 11:23:31 +0800

    1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import find_packages, setup
 setup(
     name='uitestool',
-    version='3.0.0',
+    version='3.1.1',
     description='a automated test tools',
     author='szmgithubcode',
     author_email='13023297816@163.com',
